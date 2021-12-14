b3b4da88360a138716d38098e97b334c5e0dce89
iaminsleep <ruslanergashevv@yandex.ru>
2021-12-15 01:45:34 +0300

    превращение App.js в класс, добавлены первые методы и состояния

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import React from 'react';
+import React, {Component} from 'react';
 //демонстрация возможностей импорта функций
 import Total /*имя может быть любым, без фигурных скобок т.к export default*/ from './components/total/Total';
 import {History} /*имя строго, фигурные скобки, т.к export default*/ from './components/history/History';
 import Operations /*имя может быть любым, без фигурных скобок, т.к export default*/ from './components/operation/Operation';
 
-function App() {
-  return (
-    <>
-      <header>
-        <h1>Кошелек</h1>
-        <h2>Калькулятор расходов</h2>
-      </header>
-      <main>
-          <div className="container">
-              <Total/>
-              <History/>
-              <Operations/>
-          </div>
-      </main>
-    </>
-  );
+class App extends Component { //классы позволяют хранить состояния
+
+  constructor(props) { //здесь хранятся состояния
+    super(props);
+    this.state = {
+      transactions: [],
+      description: '',
+      moneyAmount: '',
+    }
+    this.addAmount = this.addAmount.bind(this); //решение проблемы с setState undefined
+  }
+
+  //именно этот метод мы передаем в Operation
+  addTransaction(add) {
+    const transaction = {
+      id: `cmr${(+new Date).toString(16)}key`, //+ перед new Date нужен чтобы дата сразу переводилась в число. 16 переводит число в шестнадцатеричную систему
+      description: this.state.description,
+      moneyAmount: this.state.moneyAmount,
+      add
+    }
+  }
+
+  addAmount(e) { //это асинхронная функция
+    this.setState({moneyAmount: e.target.value}, /*далее идёт callback-функция(действие после)*/ ()=>{console.log(this.state)}) 
+  }
+
+  addDescription(e) { //это асинхронная функция
+    this.setState({description: e.target.value}) 
+  }
+
+  render() {
+    return (
+      /*эти треугольные скобки могут быть пустыми, т.к по умолчанию и так стоит React.Fragment*/
+      <React.Fragment> 
+        <header>
+          <h1>Кошелек</h1>
+          <h2>Калькулятор расходов</h2>
+        </header>
+        <main>
+            <div className="container">
+                <Total/>
+                <History/>
+                <Operations 
+                  addTransaction={this.addTransaction}
+                  addAmount={this.addAmount}
+                />
+            </div>
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
--- src/components/operation/Operation.js
+++ src/components/operation/Operation.js
@@ -1,19 +1,28 @@
 import React from 'react';
 
-export default function Operation() {
+export default function Operation({addTransaction, addDescription, addAmount}) { //деструктуризировали props и получили сразу нужную функцию
   return(
       <section className="operation">
       <h3>Новая операция</h3>
       <form id="form">
           <label>
-              <input type="text" className="operation__fields operation__name" placeholder="Наименование операции"/>
+              <input 
+                onChange={addDescription}
+                type="text" 
+                className="operation__fields operation__name" 
+                placeholder="Наименование операции"
+              />
           </label>
           <label>
-              <input type="number" className="operation__fields operation__amount" placeholder="Введите сумму"/>
+              <input onChange={addAmount} type="number" className="operation__fields operation__amount" placeholder="Введите сумму"/>
           </label>
           <div className="operation__btns">
-              <button type="submit" className="operation__btn operation__btn-subtract">РАСХОД</button>
-              <button type="submit" className="operation__btn operation__btn-add">ДОХОД</button>
+              <button 
+                onClick={() => addTransaction(false)} 
+                type="button" 
+                className="operation__btn operation__btn-subtract">РАСХОД</button>
+              <button 
+              onClick={() => addTransaction(true)} type="button" className="operation__btn operation__btn-add">ДОХОД</button>
           </div>
       </form>
   </section>
